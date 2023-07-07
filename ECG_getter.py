8c84545e0be2220e9d3ab4644e931984b78e1e25
namenaro <nanenaro@gmail.com>
2023-07-07 15:25:21 +0300

    pkjhbth

--- ECG_getter.py
+++ ECG_getter.py
@@ -37,10 +37,10 @@ def get_signal():
 def get_mini_ECG():
     full_signal = get_signal()
     step = 10
-    new_len = 85
+    new_len = 88
     new_indexes = list(range(0,  len(full_signal), step))
     new_signal = list([full_signal[i] for i in new_indexes])
-    return new_signal[0:85]
+    return new_signal[0:new_len]
 
 if __name__ == "__main__":
     print(get_signal())
--- draw_ECG.py
+++ draw_ECG.py
@@ -3,10 +3,27 @@ from mpl_toolkits.axisartist.axislines import SubplotZero
 from ECG_getter import get_signal, get_mini_ECG
 
 
+def make_arrows(axs):
+    axs.xaxis.set_ticks_position('bottom')
+    axs.yaxis.set_ticks_position('left')
+
+    # make arrows
+    axs.spines['left'].set_position('zero')
+    axs.spines['right'].set_visible(False)
+    axs.spines['bottom'].set_position('zero')
+    axs.spines['top'].set_visible(False)
+    axs.xaxis.set_ticks_position('bottom')
+    axs.yaxis.set_ticks_position('left')
+    axs.plot((1), (0), ls="", marker=">", ms=10, color="k",
+             transform=axs.get_yaxis_transform(), clip_on=False)
+    axs.plot((0), (1), ls="", marker="^", ms=10, color="k",
+             transform=axs.get_xaxis_transform(), clip_on=False)
+
+
 def draw_on_ax(ax, ecg):
     signal = ecg
     ax.plot(signal)
-    #ax.set_xticks(range(len(signal)))
+    ax.set_xticks(range(0,len(signal),5))
 
 
 if __name__ == "__main__":
@@ -14,4 +31,5 @@ if __name__ == "__main__":
 
     fig, axs = plt.subplots()
     draw_on_ax(axs, signal)
+    make_arrows(axs)
     plt.show()
